fb1d3b1090861f47b23289c8257fa4680095a85d
eyuelyesew21 <eyuelyesew21@gmail.com>
2026-05-28 06:33:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("TNG Platform Started");
+const supabaseUrl = "https://dknksfcesarrvyfufdti.supabase.co";
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrbmtzZmNlc2FycnZ5ZnVmZHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTI4ODMsImV4cCI6MjA5NTQ2ODg4M30.bemPqKzMlUrK7C9bddrAuspC-JtYIfciCxi7eECQEwk";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+console.log("Supabase Connected");
